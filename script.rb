e5a48f41f5ebf1ce0e50d15b4090f9e3a7b952e3
Alexei <alexeibradshaw12@gmail.com>
2022-05-30 13:25:02 +0100

    Added elsif for space character, to ensure a space remains a space in the coded string

--- script.rb
+++ script.rb
@@ -1,22 +1,21 @@
 def cipher(user_string_input, user_cipher = 0)
-    character_bytes = user_string_input.bytes
-    character_after_alteration = []
-      character_bytes.each do |character|
-        newChar = character
-         for item in 1..user_cipher do
-           if newChar == 122
-             newChar = (97)
-           elsif newChar == 90
-             newChar = (65)
-           else
-             newChar += 1
-           end
+  character_bytes = user_string_input.bytes
+  character_after_alteration = []
+    character_bytes.each do |character|
+      newChar = character
+       for item in 1..user_cipher do
+         if newChar == 122
+           newChar = 97
+         elsif newChar == 90
+           newChar = 65
+         elsif newChar == 32
+           newChar = 32
+         else
+           newChar += 1
          end
-        character_after_alteration.push newChar
-      end
-    
-    coded_char = character_after_alteration.pack('C*')
-    p "Original #{user_string_input}, coded_char: #{coded_char}"
-  end
-  
-  
+       end
+      character_after_alteration.push newChar
+    end
+  coded_char = character_after_alteration.pack('C*')
+  puts "Original #{user_string_input}, coded_char: #{coded_char}"
+end
