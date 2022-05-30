b1686a157af3f42de61010190f6fe1b4ab252bb7
Alexei <alexeibradshaw12@gmail.com>
2022-05-30 13:30:54 +0100

    Added an output example

--- script.rb
+++ script.rb
@@ -17,5 +17,8 @@ def cipher(user_string_input, user_cipher = 0)
       character_after_alteration.push newChar
     end
   coded_char = character_after_alteration.pack('C*')
-  puts "Original #{user_string_input}, coded_char: #{coded_char}"
 end
+
+message = "Hunter x hunter is the best show yep"
+
+puts "Original message: #{message}, cipher: #{cipher(message, 3)}"
