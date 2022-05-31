d8dba65ecfc3074e54ca5963199e29a80b5ec3de
Alexei <alexeibradshaw12@gmail.com>
2022-05-31 16:55:07 +0100

    Changed logic to allow for all other characters aside from letters to remain the same

--- script.rb
+++ script.rb
@@ -1,24 +1,18 @@
 def cipher(user_string_input, user_cipher = 0)
-  character_bytes = user_string_input.bytes
-  character_after_alteration = []
-    character_bytes.each do |character|
-      newChar = character
-       for item in 1..user_cipher do
-         if newChar == 122
-           newChar = 97
-         elsif newChar == 90
-           newChar = 65
-         elsif newChar == 32
-           newChar = 32
-         else
-           newChar += 1
-         end
-       end
-      character_after_alteration.push newChar
+  result = ''
+  user_string_input.each_char do |c|
+    char_as_ord = c.ord
+    p char_as_ord
+    minVal = char_as_ord < 91 ? 65 : 97
+    if char_as_ord.between?(65, 90) || char_as_ord.between?(97, 122)
+      result += ((((char_as_ord - minVal) + user_cipher) % 26) + minVal).chr
+    else
+      result += c
     end
-  coded_char = character_after_alteration.pack('C*')
+  end
+  result
 end
 
-message = "Hunter x hunter is the best show yep"
+message = "azAZ"
 
 puts "Original message: #{message}, cipher: #{cipher(message, 3)}"
